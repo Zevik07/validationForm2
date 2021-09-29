5ef683bcb31b57c0ba54004af0d8779ffca7c0ab
JC Naty <monnatyz@gmail.com>
2021-09-29 19:27:00 +0700

    update validator

--- assets/js/validator.js
+++ assets/js/validator.js
@@ -3,6 +3,7 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 function Validator(formSelector) { 
+    let _this = this;
     // Get form  
     let formElement = $(formSelector);
     // Stop when form's not found
@@ -17,7 +18,7 @@ function Validator(formSelector) {
         },
         email: function (value, msg = 'Email không đúng định dạng') {  
             regexEmail = /^\S+@\S+\.\S+$/
-            return value.test(regexEmail) ? undefined : msg;
+            return regexEmail.test(value) ? undefined : msg;
         },
         min: function (min) {
             return function (value, msg = `Trường này có ít nhất ${min} ký tự`) {
@@ -47,9 +48,88 @@ function Validator(formSelector) {
             if (!Array.isArray(formRules[input.name])){
                 formRules[input.name] = [ruleFunc];
             }
-            else 
+            else {
                 formRules[input.name].push(ruleFunc);
+            }
+        }
+        // Event
+        input.onblur = handleValidate;
+        input.oninput = handleValidate;
+    }
+    function handleValidate(e) {  
+        let rules = formRules[e.target.name];
+        let formGroup = e.target.closest('.form-group');
+        let formMessage = formGroup.querySelector('.form-message');
+        let errorMessage;
+        rules.forEach(rule => {
+            errorMessage = rule(e.target.value);
+            return;
+        });
+        
+        if (errorMessage) {
+            if (!formGroup || !formMessage) return;
+            formGroup.classList.add('invalid');
+            formMessage.innerText = errorMessage;
+        }  
+        else {
+            formGroup.classList.remove('invalid');
+            formMessage.innerText = '';
+        }
+        // Return true if errorMessage is undefined
+        return !errorMessage;
+    }
+    // Submit behavior
+    formElement.onsubmit = function (e) {  
+        e.preventDefault();
+        let isValid = true;
+        inputs.forEach(input => {
+            isValid = !handleValidate({target: input}) ? false : true;
+        });
+        // If form is valid, it will be submit
+        if (isValid)
+        {
+            if (typeof _this.onSubmit == 'function')
+            {
+                let data = getFormData();
+                _this.onSubmit(data);
+            }
+            else {
+                formElement.submit();
+            }
         }
     }
-    console.log(formRules);
+    // Get form data
+    function getFormData() {  
+        // Get form input
+        let formValues = Array.from(inputs).reduce((values, input) => {
+            switch (input.type) {
+                case 'radio':
+                    if (input.matches(':checked')){
+                        values[input.name] = input.value
+                    }
+                    else {
+                        values[input.name] = ''
+                    }
+                    break;
+                case 'checkbox':
+                    if (!input.matches(':checked')) {
+                        values[input.name] = ''
+                        return values
+                    }
+                    if (!Array.isArray(values[input.name])){
+                        values[input.name] = []
+                    }
+                    values[input.name].push(input.value)
+                    break;
+                case 'file':
+                    values[input.name] = input.files
+                    break;
+                default:
+                    values[input.name] = input.value
+                    break;
+            }
+            return values
+        }, {})
+        return formValues;
+    }
 }
